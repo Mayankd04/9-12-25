eb7d88fa3a8102cf43ca505fb38663c08fdcc047
Mayankd04 <mayankdewangan9891@gmail.com>
2025-12-09 09:48:36 +0530

    hi their !

---
+++ 2nd.c
@@ -0,0 +1,18 @@
+//Draw flowcharts for sum & largest of two numbers, Implement the program in C, Show VS Code shortcuts any 5.
+
+#include<stdio.h>
+int main() {
+    printf("\n Mayank Dewangan erp-id RU-25-10793\n\n");
+    int a,b;
+    printf("Enter the first number: ");
+    scanf("%d",&a);
+    printf("\n Enter the second number: ");
+    scanf("%d",&b);
+    if (a>b){
+        printf("%d is greater then %d",a,b);
+    }
+    else{printf("%d is greater then %d",b,a);
+    }
+
+    return 0;
+}
